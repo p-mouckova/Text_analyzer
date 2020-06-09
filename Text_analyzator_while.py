0a7c5e59852b29cf72da699d1eff93955071c28c
Pavla Mouckova <mouckova.pavla@gmail.com>
2020-06-09 10:56:35 +0200

    This is my second commit. Uploading to branch "development" my second version.

--- Text_analyzator_while.py
+++ Text_analyzator_while.py
@@ -1,59 +1,59 @@
-TEXTS = ['''
-Situated about 10 miles west of Kemmerer, 
-Fossil Butte is a ruggedly impressive 
-topographic feature that rises sharply 
-some 1000 feet above Twin Creek Valley 
-to an elevation of more than 7500 feet 
-above sea level. The butte is located just 
-north of US 30N and the Union Pacific Railroad, 
-which traverse the valley. ''',
-
-'''At the base of Fossil Butte are the bright 
-red, purple, yellow and gray beds of the Wasatch 
-Formation. Eroded portions of these horizontal 
-beds slope gradually upward from the valley floor 
-and steepen abruptly. Overlying them and extending 
-to the top of the butte are the much steeper 
-buff-to-white beds of the Green River Formation, 
-which are about 300 feet thick.''',
-
-'''The monument contains 8198 acres and protects 
-a portion of the largest deposit of freshwater fish 
-fossils in the world. The richest fossil fish deposits 
-are found in multiple limestone layers, which lie some 
-100 feet below the top of the butte. The fossils 
-represent several varieties of perch, as well as 
-other freshwater genera and herring similar to those 
-in modern oceans. Other fish such as paddlefish, 
-garpike and stingray are also present.'''
+TEXTS = [
+    '''Situated about 10 miles west of Kemmerer, 
+    Fossil Butte is a ruggedly impressive 
+    topographic feature that rises sharply 
+    some 1000 feet above Twin Creek Valley 
+    to an elevation of more than 7500 feet 
+    above sea level. The butte is located just 
+    north of US 30N and the Union Pacific Railroad, 
+    which traverse the valley. ''',
+    '''At the base of Fossil Butte are the bright 
+    red, purple, yellow and gray beds of the Wasatch 
+    Formation. Eroded portions of these horizontal 
+    beds slope gradually upward from the valley floor 
+    and steepen abruptly. Overlying them and extending 
+    to the top of the butte are the much steeper 
+    buff-to-white beds of the Green River Formation, 
+    which are about 300 feet thick.''',
+    '''The monument contains 8198 acres and protects 
+    a portion of the largest deposit of freshwater fish 
+    fossils in the world. The richest fossil fish deposits 
+    are found in multiple limestone layers, which lie some 
+    100 feet below the top of the butte. The fossils 
+    represent several varieties of perch, as well as 
+    other freshwater genera and herring similar to those 
+    in modern oceans. Other fish such as paddlefish, 
+    garpike and stingray are also present.'''
 ]
 
-USERS = {'bob':'123',
-         'ann':'pass123',
-         'mike':'password123',
-         'liz':'pass123'}
+USERS = {
+    'bob': '123',
+    'ann': 'pass123',
+    'mike': 'password123',
+    'liz': 'pass123'
+}
 
 ODDELOVAC = '-' * 40
 
 print(ODDELOVAC)
 print('Welcome to the app. Please log in: ')
-USERNAME = input('USERNAME: ')
-PASSWORD = input('PASSWORD: ')
+username = input('USERNAME: ')
+password = input('PASSWORD: ')
 print(ODDELOVAC)
 
-if USERS.get(USERNAME) != PASSWORD:
+if USERS.get(username) != password:
     print('Incorrect USERNAME or PASSWORD. Please log in again.')
     exit()
 
 print('We have 3 texts to be analyzed.')
-NUMBER = int(input('Enter a number btw. 1 and 3 to select: '))
+number = input('Enter a number btw. 1 and 3 to select: ')
 print(ODDELOVAC)
 
-if NUMBER < 1 or NUMBER > 3:
-    print('Incorrect NUMBER! Please select number 1,2 or 3.')
+if not number.isdigit() or int(number) < 1 or int(number) > 3:
+    print('You did not enter a correct number.')
     exit()
 
-TEXT = str(TEXTS[NUMBER-1])
+TEXT = TEXTS[int(number)-1]
 words_with_signs = TEXT.split()
 words = []
 while words_with_signs:
@@ -83,7 +83,7 @@ while i < len(words):
         numeric += 1
 
     lenght = len(words[i])
-    word_lenghts[lenght] = word_lenghts.get(lenght,0) + 1
+    word_lenghts[lenght] = word_lenghts.get(lenght, 0) + 1
     i += 1
 
 print('There are', titlecase, 'titlecase words')
@@ -99,7 +99,7 @@ while i < len(counts_letters):
     word = counts_letters[i]
     if word in word_lenghts:
         frequency = word_lenghts.get(word)
-        print(word,'*' * frequency, frequency)
+        print(word, '*' * frequency, frequency)
     i += 1
 
 print(ODDELOVAC)
